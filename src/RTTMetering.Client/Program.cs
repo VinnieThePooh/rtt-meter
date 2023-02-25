582b58e3a4894efe2912a08931eebe4408516675
Lashkevich Ryan <lashkevichrain@gmail.com>
2023-02-25 18:07:37 +0700

    nchore(): copied RTT client and server projects from Streaming.Shared

--- src/RTTMetering.Client/Program.cs
+++ src/RTTMetering.Client/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Sockets;
+using DataStreaming.Constants.RTT;
+using DataStreaming.Exceptions;
+using DataStreaming.Protocols.Factories;
+using DataStreaming.Services.Interfaces;
+using DataStreaming.Services.RTT;
+using DataStreaming.Settings;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+            
+        var meteringSettings = configuration.GetSection(RttMeteringSettings.SectionName).Get<RttMeteringSettings>();
+        var protoFactory = (IRttMeteringProtocolFactory)RttMeteringProtocolFactory.Create();
+        
+        IRttMeteringService meteringService = new RttMeteringService(meteringSettings, protoFactory);
+        meteringService.RttReceived += (o, eventArgs) =>
+        {
+              
+            if (eventArgs.MeteringType == RttMeteringType.SinglePacket)
+            {
+                var stats = eventArgs.RttStats.Value;
+                Console.WriteLine($"[{nameof(RttMeteringType.SinglePacket)}]: {stats.SequenceNumber}. {Math.Ceiling(stats.RttValue.TotalMicroseconds)} mks");
+                // Console.WriteLine($"[{nameof(RttMeteringType.SinglePacket)}]: {stats.SequenceNumber}. {stats.RttValue}");
+                return Task.CompletedTask;
+            }
+        
+            var agStats = eventArgs.AggregatedRttStats.Value;
+            Console.WriteLine($"[{nameof(RttMeteringType.AggregationInterval)}]: {agStats.SequenceNumber}. " +
+                                $"Average val: {agStats.RttValue.TotalMicroseconds} mks (({agStats.PacketsCount} for {agStats.AggregationInterval} ms))");
+            return Task.CompletedTask;
+        };
+        try
+        {
+            _ = await meteringService.Start();
+        }
+        catch (SocketException e)
+        {
+            //todo: log here
+            Console.WriteLine(e);
+            Environment.Exit(e.ErrorCode);
+        }
+        catch (DisconnectedPrematurelyException e)
+        {
+            //todo: log here
+            Console.WriteLine($"RTT echo server disconnected prematurely:\n{e}");
+            Environment.Exit(e.HResult);
+        }
+        catch (Exception e)
+        {
+            //todo: log here
+            Console.WriteLine($"Unexpected exception:\n{e}");
+            Environment.Exit(e.HResult);
+        }
+        finally
+        {
+            await meteringService.DisposeAsync();
+        }
--- src/RTTMetering.Server/Program.cs
+++ src/RTTMetering.Server/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Sockets;
+using DataStreaming.Protocols.Factories;
+using DataStreaming.Services.RTT;
+using DataStreaming.Settings;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var hostSettings = configuration.GetSection(HostSettings.SectionName).Get<HostSettings>();
+var protoFactory = (IRttMeteringProtocolFactory)RttMeteringProtocolFactory.Create();
+var server = new RttMeteringServer(hostSettings, protoFactory);
+server.ListeningStarted += (o, eventArgs) =>
+{
+    Console.WriteLine($"[{nameof(RttMeteringServer)}]: Listening at {eventArgs.EndPoint}");
+    return Task.CompletedTask;
+};
+
+try
+{
+    _ = await server.Start();
+}
+catch (SocketException e)
+{
+    //todo: log here
+    Console.WriteLine(e);
+    await server.DisposeAsync();
+    Environment.Exit(e.ErrorCode);
+}
+catch (Exception e)
+{
+    //todo: log here
+    Console.WriteLine($"Unexpected exception: {e}");
+    Environment.Exit(e.HResult);
+}
+finally
+{
+    await server.DisposeAsync();
+}
